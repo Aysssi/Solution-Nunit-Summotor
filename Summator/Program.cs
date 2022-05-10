588702ba343145d517f70bcc6f4a6d8aa0505ba9
Aysel Chausheva <102178248+Aysssi@users.noreply.github.com>
2022-05-10 11:11:00 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6 });
+            if (result == 21)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+
+            double average = Summator.Average(new long[] { 5, 10, 15 });
+            if (average ==10)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+
+
+        }
+    }
+}
